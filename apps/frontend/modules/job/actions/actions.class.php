5bb6f37cc3b030f375860998bd09fb6f6a585610
Fabien Strebel <fabien@strebel.fr>
2010-05-08 23:10:12 +0200

    Object serialization

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,9 +12,11 @@ class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
-    $this->jobeet_jobs = Doctrine::getTable('JobeetJob')
-      ->createQuery('a')
-      ->execute();
+    $q = Doctrine_Query::create()
+      ->from('JobeetJob j')
+      ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
+
+    $this->jobeet_jobs = $q->execute();
   }
 
   public function executeShow(sfWebRequest $request)
