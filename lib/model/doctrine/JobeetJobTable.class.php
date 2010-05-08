2a503539fa24b11f37644e1aebe500262a1758df
Fabien Strebel <fabien@strebel.fr>
2010-05-08 23:37:59 +0200

    orderBy

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -11,7 +11,8 @@ class JobeetJobTable extends Doctrine_Table
     public function getActiveJobs()
     {
       $q = $this->createQuery('j')
-        ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
+        ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+        ->orderBy('j.expires_at DESC');
 
       return $q->execute();
     }
