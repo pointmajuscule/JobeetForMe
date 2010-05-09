763491ae496082ddf962d7dbe7dc085d78084e84
Fabien Strebel <fabien@strebel.fr>
2010-05-09 22:23:25 +0200

    dernière amélioration des tests

--- lib/Jobeet.class.php
+++ lib/Jobeet.class.php
@@ -4,16 +4,16 @@ class Jobeet
 {
   static public function slugify($text)
   {
-    if (empty($text))
-    {
-      return 'n-a';
-    }
-
     //replace all non letters or digits by -
     $text = preg_replace('/\W+/', '-', $text);
 
     //trim and lowercase
     $text = strtolower(trim($text, '-'));
+
+    if (empty($text))
+    {
+      return 'n-a';
+    }
 
     return $text;
   }
--- test/unit/JobeetTest.php
+++ test/unit/JobeetTest.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__).'/../bootstrap/unit.php';
 
-$t = new lime_test(7);
+$t = new lime_test(8);
 
 $t->comment('::slugify()');
 $t->is(Jobeet::slugify('Sensio'), 'sensio', '::slugify() converts all characters to lower case');
@@ -12,3 +12,4 @@ $t->is(Jobeet::slugify('paris,france'), 'paris-france', '::slugify() replace non
 $t->is(Jobeet::slugify('  sensio'), 'sensio', '::slugify() removes - at the beginning of a string');
 $t->is(Jobeet::slugify('sensio  '), 'sensio', '::slugify() removes - at the end of a string');
 $t->is(Jobeet::slugify(''), 'n-a', '::slugify() converts the empty string to n-a');
+$t->is(Jobeet::slugify(' - '), 'n-a', '::slugify() converts a string that only contains non-ASCII characters to n-a');
