15f4dcde75ed7f258999dce00c73329ade825d38
Fabien Strebel <fabien@strebel.fr>
2010-05-10 22:57:53 +0200

    Test Data

---
+++ lib/test/JobeetTestFunctional.class.php
@@ -0,0 +1,10 @@
+<?php
+class JobeetTestFunctional extends sfTestFunctional
+{
+  public function loadData()
+  {
+    Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/fixtures');
+
+    return $this;
+  }
+}
