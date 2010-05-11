2904c6aa3b2d051dc73dbab130282daa6f12ee33
Fabien Strebel <fabien@strebel.fr>
2010-05-11 12:18:05 +0200

    liste de choix dans le modèle

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -3,6 +3,17 @@
 
 class JobeetJobTable extends Doctrine_Table
 {
+  static public $types = array(
+    'full-time' => 'Full time',
+    'part-time' => 'Part time',
+    'freelance' => 'Freelance',
+  );
+
+  public function getTypes()
+  {
+    return sefl::$types;
+  }
+
     public static function getInstance()
     {
         return Doctrine_Core::getTable('JobeetJob');
