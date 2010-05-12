3c0216905642369d89c27699315d7c615f6bd2f6
Fabien Strebel <fabien@strebel.fr>
2010-05-12 21:40:56 +0200

    Submitting a form

--- test/functional/frontend/jobActionsTest.php
+++ test/functional/frontend/jobActionsTest.php
@@ -81,3 +81,31 @@ $browser->info('2 - The job page')->
 $browser->getExpiredJob()->getId()))->
   with('response')->isStatusCode(404)
 ;
+
+$browser->info('3 - Post a Job page')->
+  info(' 3.1 - Submit a Job')->
+
+  get('/job/new')->
+  with('request')->begin()->
+    isParameter('module', 'job')->
+    isParameter('action', 'new')->
+  end()->
+
+  click('Preview your job', array('job' => array(
+    'company'     => 'Sensio Labs',
+    'url'         => 'http://www.sensio.com/',
+    'logo'        => sfConfig::get('sf_upload_dir').'/jobs/sensio-labs.gif',
+    'position'    => 'Developer',
+    'location'    => 'Atlanta, USA',
+    'description' => 'You will work with symfony to develop websites for our
+      customers.',
+    'how_to_apply' => 'Send me an email',
+    'email'       => 'for.a.job@example.com',
+    'is_public'   => false,
+  )))->
+
+  with('request')->begin()->
+    isParameter('module', 'job')->
+    isParameter('action', 'create')->
+  end()
+;
