b5522140f4c991b92ca432d0e572cff871ec493b
Fabien Strebel <fabien@strebel.fr>
2010-05-12 22:00:43 +0200

    Eviter la multiplication

--- lib/test/JobeetTestFunctional.class.php
+++ lib/test/JobeetTestFunctional.class.php
@@ -28,4 +28,23 @@ class JobeetTestFunctional extends sfTestFunctional
 
     return $q->fetchOne();
   }
+
+  public function createJob($values = array())
+  {
+    return $this->
+      get('/job/new')->
+      click('Preview your job', array('job' => array_merge(array(
+        'company'       => 'Sensio Labs',
+        'url'           => 'http://www.sensio.com/',
+        'position'      => 'Developer',
+        'location'      => 'Atlanta, USA',
+        'description'   => 'You will work with symfony to develop websites for
+          our customers.',
+        'how_to_apply'  => 'Send me an email',
+        'email'         => 'for.a.job@example.com',
+        'is_public'     => false,
+      ), $values)))->
+      followRedirect()
+    ;
+  }
 }
