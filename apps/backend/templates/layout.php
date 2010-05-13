754a785f342f67ab0185023e1d1a27a0e8a6bf22
Fabien Strebel <fabien@strebel.fr>
2010-05-13 19:28:45 +0200

    Amélioration de l interface utilisateur du backend

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -1,14 +1,44 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
+    <title>Jobeet Admin Interface</title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="container">
+      <div id="header">
+        <h1>
+          <a href="<?php echo url_for('homepage') ?>">
+            <img src="http://www.symfony-project.org/images/logo.jpg"
+            qlt="Jobeet Job Board" />
+          </a>
+        </h1>
+      </div>
+
+      <div id="menu">
+        <ul>
+          <li>
+            <?php echo link_to('Jobs', 'jobeet_job') ?>
+          </li>
+          <li>
+            <?php echo link_to('Categories', 'jobeet_category') ?>
+          </li>
+        </ul>
+      </div>
+
+      <div id="content">
+        <?php echo $sf_content ?>
+      </div>
+
+      <div id="footer">
+        <img src="/images/jobeet-mini.png" />
+        powered by <a href="http://www.symfony-project.org/">
+          <img src="/images/symfony.gif" alt ="symfony framework" /></a>
+      </div>
+    </div>
+  </body>
+</html>
   </body>
 </html>
