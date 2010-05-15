6254d0d8fb5ebcca41ed548c8b1a1a6149c88194
Fabien Strebel <fabien@strebel.fr>
2010-05-15 11:08:41 +0200

    création du module api

---
+++ apps/frontend/modules/api/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * api actions.
+ *
+ * @package    jobeet
+ * @subpackage api
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class apiActions extends sfActions
+{
+}
