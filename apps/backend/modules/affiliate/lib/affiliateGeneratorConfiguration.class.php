60787338f740c097aee6ac7249fc3120145214c3
Fabien Strebel <fabien@strebel.fr>
2010-05-15 19:40:39 +0200

    création du backend

---
+++ apps/backend/modules/affiliate/lib/affiliateGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * affiliate module configuration.
+ *
+ * @package    jobeet
+ * @subpackage affiliate
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class affiliateGeneratorConfiguration extends BaseAffiliateGeneratorConfiguration
+{
+}
