a636cfcc68e5548303db53f22714282b713b0173
Fabien Strebel <fabien@strebel.fr>
2010-05-15 15:06:01 +0200

    suite...

--- apps/frontend/modules/api/actions/actions.class.php
+++ apps/frontend/modules/api/actions/actions.class.php
@@ -12,7 +12,19 @@ class apiActions extends sfActions
 {
   public function executeList(sfWebRequest $request)
   {
-    $this->jobs[$this->generateUrl('job_show_user', $job, true)] =
-      $job->asArray($request->getHost());
+    $this->jobs = array();
+    foreach ($this->getRoute()->getObjects() as $job)
+    {
+      $this->jobs[$this->generateUrl('job_show_user', $job, true)] =
+        $job->asArray($request->getHost());
+    }
+
+    switch ($request->getRequestFormat())
+    {
+      case 'yaml':
+        $this->setLayout(false);
+        $this->getResponse()->setContentType('text/yaml');
+        break;
+    }
   }
 }
