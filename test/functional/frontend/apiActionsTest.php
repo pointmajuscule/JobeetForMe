c081ec98a2ce00ce73760cfc1ad40da05d05e485
Fabien Strebel <fabien@strebel.fr>
2010-05-15 15:25:47 +0200

    les tests des web services

--- test/functional/frontend/apiActionsTest.php
+++ test/functional/frontend/apiActionsTest.php
@@ -2,18 +2,37 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new JobeetTestFunctional(new sfBrowser());
 
-$browser->
-  get('/api/index')->
+$browser->loadData();
 
-  with('request')->begin()->
-    isParameter('module', 'api')->
-    isParameter('action', 'index')->
+$browser->info('1 - Web service security')->
+
+  info(' 1.1 - A token is needed to access the service')->
+  get('/api/foo/jobs.xml')->
+  with('response')->isStatusCode(404)->
+
+  info(' 1.2 - An inactive account cannot access the web service')->
+  get('/api/symfony/jobs.xml')->
+  with('response')->isStatusCode(404)->
+
+info('2 - The jobs returned are limited to the categories configured for the affiliate')->
+  get('/api/sensio_labs/jobs.xml')->
+  with('request')->isFormat('xml')->
+  with('response')->begin()->
+    isValid()->
+    checkElement('job', 32)->
   end()->
 
+info('3 - The web service supports the JSO? format')->
+  get('/api/sensio_labs/jobs.json')->
+  with('request')->isFormat('json')->
+  with('response')->matches('/"category"\: "Programming"/')->
+
+info('4 - The web service supports the YAML format')->
+  get('/api/sensio_labs/jobs.yaml')->
   with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
+    isHeader('content-type', 'text/yaml; charset=utl-8')->
+    matches('/category\: Programming/')->
   end()
 ;
